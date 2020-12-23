751c6aa67cb638f73d817d00c3f28d7f60fabcc9
Shun Zhang <shunz@umich.edu>
2020-12-22 23:42:12 -0600

    Added Kulfan airfoils (generation and inversion). Refactored Parsec airfoils

---
+++ kulfan_airfoil.py
@@ -0,0 +1,198 @@
+from math import factorial
+import numpy as np
+import scipy.optimize
+
+
+def get_airfoil_coord(wu, wl, yute, ylte, x=None):
+    # N1 and N2 parameters (N1 = 0.5 and N2 = 1 for airfoil shape)
+    N1 = 0.5
+    N2 = 1
+
+    # Create x coordinate
+    if x is None:  # if x_arr is not provided
+        N = 161  #TODO: hard coded for now
+        x = 0.5 * (1 + np.cos(np.linspace(0, 2*np.pi, N)))  # todo: 1 TO 0 TO 1
+
+    j_le = np.argmin(x)
+    xu = x[:j_le]  # upper surface x-coordinates
+    xl = x[j_le:]  # lower surface x-coordinates
+
+    yu = eval_shape_fcn(wu, xu, N1, N2, yute)  # Call ClassShape function to determine upper surface y-coordinates
+    yl = eval_shape_fcn(wl, xl, N1, N2, ylte)  # Call ClassShape function to determine lower surface y-coordinates
+
+    y = np.concatenate([yu, yl])  # Combine upper and lower y coordinates
+
+    return np.array([x, y]).T
+
+
+def eval_shape_fcn(w, x, N1, N2, yte):
+    """
+    compute class and shape function
+    :param w:
+    :param x:
+    :param N1:
+    :param N2:
+    :param yte: trailing edge y coordinate
+    :return:
+    """
+
+    # class function; taking input of N1 and N2
+    C = x**N1 * (1-x)**N2
+
+    n = len(w) - 1  # degree of Bernstein polynomials
+
+    S = np.zeros_like(x)
+    for j in range(0, n+1):
+        K = factorial(n)/(factorial(j)*(factorial(n-j)))
+        S += w[j]*K*x**j * ((1-x)**(n-j))
+
+    return C * S + x * yte
+
+
+def list_to_kulfan_params(var):
+    nw = len(var) // 2
+    wu = var[0:nw]
+    wl = var[nw:(2*nw)]
+
+    assert len(var) == (2 * nw)
+    return wu, wl
+
+    # dz = var[2*nw]
+    # assert len(var) == (2*nw+1)
+    # return wu, wl, dz
+
+
+def obj_fcn_airfoil_inversion(var, xy_ref):
+    """
+    objective function to minimize: discrepancy between reference coordinates and parametrized airfoil coordinates
+    :param var: variable to optimize
+    :param xy_ref: n-by-2 array of reference coordinates following XFoil format (CCW)
+    :return:
+    """
+    # wu, wl, dz = list_to_kulfan_params(var)
+
+    wu, wl = list_to_kulfan_params(var)
+    yute = xy_ref[0, 1]
+    ylte = xy_ref[-1, 1]
+
+    xy_kulfan = get_airfoil_coord(wu, wl, yute, ylte, x=xy_ref[:, 0])
+
+    # scale for normalization
+    y_scale = np.max(xy_ref[: 1]) - np.min(xy_ref[:, 1])
+    # y_scale = 1e-2  # an arbitrary scale
+    # y_scale = np.maximum(1e-2, np.fabs(xy_ref[:, 1]))
+    # y_scale = np.max(xy_ref[: 1]) - np.min(xy_ref[:, 1]) * (2 - np.cos(2*pi*xy_ref[:, 0]/xte))
+
+    npts = xy_ref.shape[0]
+
+    return np.sum(((xy_ref[:, 1] - xy_kulfan[:, 1]) / y_scale)**2.0) / npts
+
+
+def infer_kulfan_params(xy_ref, var_init):
+    """
+    infer kulfan parameters given reference/target airfoil coordinates
+    """
+    nvar = len(var_init)
+    lb = [-2.] * nvar
+    ub = [2.] * nvar
+
+    if True:
+        bounds = scipy.optimize.Bounds(lb, ub, keep_feasible=False)
+        options_opt = {"maxiter": 1e6}
+
+        opt_method = "SLSQP"
+        # opt_method = "L-BFGS-B"
+        # opt_method = "TNC"
+        opt_result = scipy.optimize.minimize(obj_fcn_airfoil_inversion, var_init, args=xy_ref,
+                                             method=opt_method, tol=1e-16, bounds=bounds, options=options_opt)
+    else:
+        obj_fcn = lambda x: obj_fcn_airfoil_inversion(x, xy_ref)
+        opt_result = scipy.optimize.dual_annealing(obj_fcn, bounds=list(zip(lb, ub)), seed=1234)
+
+    print(f"Optimization result:\n{opt_result}")
+
+    return list_to_kulfan_params(opt_result.x)
+
+
+def main():
+    import sys
+    if sys.platform == 'darwin':  # MacOS
+        import matplotlib
+        matplotlib.use('Qt5Agg')
+    import matplotlib.pyplot as plt
+    import os
+
+    path = './data/'
+
+    # reference coordinates
+    # airfoilname = "RAE2822"
+    # airfoilname = "b737a-il"
+    airfoilname = "e387"
+    # airfoilname = "whitcomb"
+    # airfoilname = "nasasc2-0714"
+    fname_input_airfoil = os.path.join(path, f"{airfoilname}.txt")
+    xy_ref = np.genfromtxt(fname_input_airfoil, dtype=float, skip_header=1)
+
+    airfoilname_inverse = f"{airfoilname}_kulfan"
+
+    # fix trailing edge y coordinates based on reference airfoil coordinates
+    yute = xy_ref[0, 1]
+    ylte = xy_ref[-1, 1]
+
+    poly_deg = 5
+    wu = [0.1] * (poly_deg + 1)
+    wl = [-0.1] * (poly_deg + 1)
+
+    var_init = list()
+    var_init.extend(wu)
+    var_init.extend(wl)
+
+    xy_coords_init = get_airfoil_coord(wu, wl, yute, ylte)
+    # plot initial guess of airfoil
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(xy_ref[:, 0], xy_ref[:, 1], "k.--", label="Input")
+    ax.plot(xy_coords_init[:, 0], xy_coords_init[:, 1], "-.", linewidth=2, label="Initial guess")
+
+    ax.legend()
+    ax.grid(True)
+    ax.set_xlim([0, 1])
+    ax.axis('equal')
+    ax.set_xticks(np.arange(0, 1.1, 0.1))
+    plt.show(block=True)
+
+    # infer Kulfan parameters by optimization
+    wu, wl = infer_kulfan_params(xy_ref, var_init)
+
+    # get inferred Kulfan airfoil coordinates
+    xy_coords = get_airfoil_coord(wu, wl, yute, ylte)
+
+    # save to coordinate file
+    fpath = os.path.join(path, f'{airfoilname_inverse}.dat')
+    np.savetxt(fpath, xy_coords, delimiter=" ", newline="\n")
+
+    # plot airfoil
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(xy_coords[:, 0], xy_coords[:, 1], marker=".", linewidth=2, label=airfoilname_inverse)
+    ax.plot(xy_coords_init[:, 0], xy_coords_init[:, 1], "-.", linewidth=2, label="Initial guess")
+    ax.plot(xy_ref[:, 0], xy_ref[:, 1], "k.--", label="Input")
+
+    ax.legend()
+    ax.grid(True)
+    ax.set_xlim([0, 1])
+    ax.axis('equal')
+    ax.set_xticks(np.arange(0, 1.1, 0.1))
+
+    ax.set_title(f"Kulfan airfoil with parameters: yute={yute:.4e}, ylte={ylte:.4e}\n"
+                 f"wu={wu}\nwl={wl}")
+
+    fig.tight_layout()
+
+    fig.savefig(os.path.join(path, f'{airfoilname_inverse}.png'))
+
+
+if __name__ == '__main__':
+    main()
+
+
--- parsec_airfoil.py
+++ parsec_airfoil.py
@@ -65,7 +65,7 @@ def get_coord(xte, yte, parsec_params, x_arr=None):
     :param xte:
     :param yte:
     :param parsec_params:
-    :param x_arr: an array starting from 0.0 and in an ascending order
+    :param x_arr: listed in XFoil format, i.e. counter-clockwise starting from upper trailing edge, wrapping around leading edge and ending at lower trailing edge
     :return: xy_arr: listed in XFoil format, i.e. counter-clockwise starting from upper trailing edge, wrapping around leading edge and ending at lower trailing edge
     """
 
@@ -213,3 +213,109 @@ def infer_parsec_params(xte, var_init, xy_ref):
 
     parsec_params = parsec_params_list_to_dict(opt_result.x)
     return parsec_params
+
+
+def main():
+    import sys
+    if sys.platform == 'darwin':  # MacOS
+        import matplotlib
+        matplotlib.use('Qt5Agg')
+    import matplotlib.pyplot as plt
+    import os
+
+    path = './data/'
+
+    # reference coordinates
+    airfoilname = "RAE2822"
+    # airfoilname = "b737a-il"
+    # airfoilname = "e387"
+    # airfoilname = "whitcomb"
+    # airfoilname = "nasasc2-0714"
+    fname_input_airfoil = os.path.join(path, f"{airfoilname}.txt")
+    xy_ref = np.genfromtxt(fname_input_airfoil, dtype=float, skip_header=1)
+
+    # airfoil inversion
+    airfoilname_inverse = f"{airfoilname}_parsec"
+
+    # TE & LE of airfoil
+    # xte, yte = 1.0, 0.0  # normalized to chord = 1
+    xte, yte = 0.5 * (xy_ref[0, :] + xy_ref[-1, :])
+
+    # initial guess of parsec parameters
+    parsec_params_init = dict()
+    print("FYI, here is the bounds for parsec parameters:")
+    print(f"{get_parsec_params_bounds(xte)[0]}")
+
+    if False:
+        # # the following ones are close to RAE2822
+        parsec_params_init["rle"] = 8.300e-03
+        parsec_params_init["x_pre"] = 3.441e-01
+        parsec_params_init["y_pre"] = -5.880e-02
+        parsec_params_init["d2ydx2_pre"] = 7.018e-01
+        parsec_params_init["th_pre"] = -1.
+        parsec_params_init["x_suc"] = 4.312e-01
+        parsec_params_init["y_suc"] = 6.290e-02
+        parsec_params_init["d2ydx2_suc"] = -4.273e-01
+        parsec_params_init["th_suc"] = -12
+    else:
+        # the following parameters are quite arbitrary
+        parsec_params_init["rle"] = 4.300e-01
+        parsec_params_init["x_pre"] = 5.441e-01
+        parsec_params_init["y_pre"] = -5.880e-01
+        parsec_params_init["d2ydx2_pre"] = 7.018e-01
+        parsec_params_init["th_pre"] = 1.
+        parsec_params_init["x_suc"] = 7.312e-01
+        parsec_params_init["y_suc"] = 0.02
+        parsec_params_init["d2ydx2_suc"] = -4.273e-01
+        parsec_params_init["th_suc"] = -1.
+
+    var_init = [val for val in parsec_params_init.values()]  # assuming dictionary follows the order of key addition
+
+    # plot initial guess of airfoil
+    xy_coords_init = get_coord(xte, yte, parsec_params_init)  # TODO: fix upper & lower x
+
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(xy_coords_init[:, 0], xy_coords_init[:, 1], "-.", linewidth=2, label="Initial guess")
+    ax.plot(xy_ref[:, 0], xy_ref[:, 1], "k.--", label="Input")
+
+    ax.legend()
+    ax.grid(True)
+    ax.set_xlim([0, 1])
+    ax.axis('equal')
+    ax.set_xticks(np.arange(0, 1.1, 0.1))
+    plt.show(block=True)
+
+    # infer parsec parameters by optimization
+    parsec_params_init = infer_parsec_params(xte, var_init, xy_ref)
+    xy_coords = get_coord(xte, yte, parsec_params_init)
+
+    # save to coordinate file
+    fpath = os.path.join(path, f'{airfoilname_inverse}.dat')
+    np.savetxt(fpath, xy_coords, delimiter=" ", newline="\n")
+
+    # plot airfoil
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(xy_coords[:, 0], xy_coords[:, 1], marker=".", linewidth=2, label=airfoilname_inverse)
+    ax.plot(xy_coords_init[:, 0], xy_coords_init[:, 1], "-.", linewidth=2, label="Initial guess")
+    ax.plot(xy_ref[:, 0], xy_ref[:, 1], "k.--", label="Input")
+
+    ax.legend()
+    ax.grid(True)
+    ax.set_xlim([0, 1])
+    ax.axis('equal')
+    ax.set_xticks(np.arange(0, 1.1, 0.1))
+
+    parnv = [f"{name}={val:.4e}".replace('\n', '') for name, val in parsec_params_init.items()]
+    parnv = [', '.join(parnv[:5]),
+             ', '.join(parnv[5:])]
+    ax.set_title(f"PARSEC airfoil with parameters:\n{parnv[0]}\n{parnv[1]}")
+
+    fig.tight_layout()
+
+    fig.savefig(os.path.join(path, f'{airfoilname_inverse}.png'))
+
+
+if __name__ == '__main__':
+    main()
